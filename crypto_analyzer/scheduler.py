02a8cb594e276309ded71a430614d1419d8c77cd
Jan Skurski <jan.skurski@gmail.com>
2023-02-10 17:53:02 +0100

    feat: Add continuously importing data from Binance 1m chart via scheduler.py

--- crypto_analyzer/scheduler.py
+++ crypto_analyzer/scheduler.py
@@ -1,20 +1,29 @@
 import logging
+import os
 import time
+from typing import List
 
+import django
 import schedule
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crypto_analyzer.settings")
+django.setup()
+
+from charts.tasks import update_binance_values
 
 logger = logging.getLogger(__name__)
 
+TICKERS_UPDATE_1M: List[str] = os.environ.get("TICKERS_UPDATE_1M", "").split(",")
 
-def scheduled_function():
-    print('Scheduler working')
-    #TODO fill with some jobs, eg. updating some tickers regularly
-    pass
+
+def update_binance_tickers_1m():
+    update_binance_values.delay(intervals=['1m'], tickers=TICKERS_UPDATE_1M)
 
 
 def run():
+    print(os.getcwd())
     logger.info("Scheduler process has started.")
-    schedule.every(1).minute.do(scheduled_function)
+    schedule.every(1).minute.do(update_binance_tickers_1m)
     while True:
         schedule.run_pending()
         time.sleep(15)
