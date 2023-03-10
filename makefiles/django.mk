93a5180027375cb116fd1d2584225e8bc7bbe19b
Jan Skurski <jan.skurski@gmail.com>
2023-03-10 20:37:01 +0100

    chore: Fix manage.py.win help syntax

--- makefiles/django.mk
+++ makefiles/django.mk
@@ -6,5 +6,5 @@ manage.py:  ## runs chosen command in manage.py; usage: make manage.py CMD=comma
 	$(RUN_ENVIRONMENT) $(MANAGE_PY) $(CMD)
 
 .PHONY: manage.py.win
-manage.py.win:  ## runs chosen command in manage.py; usage: make manage.py CMD=command
+manage.py.win:  ## runs chosen command in manage.py; usage: make manage.py.win CMD=command
 	$(RUN_ENVIRONMENT) $(MANAGE_PY_WIN) $(CMD)
